29a16cf72cbca165fac2c4926e4fd4fcf11d2a58
IvanKem <iv.kemaev@mail.ru>
2020-08-12 12:02:00 +0300

    paths in config_example

--- GetToApi.py
+++ GetToApi.py
@@ -1,84 +1,137 @@
 # -*- coding: utf-8 -*-
 import requests
-import config_example #your config file with all keys and with export
+import config_example
 import config
 import json
 from pprint import pprint
 from urllib.request import urlretrieve
 import os
 
-#get all projects id
-response = requests.get('http://api.tildacdn.info/v1/getprojectslist/?publickey=%s&secretkey=%s' % (config.publickey, config.secretkey))
+
+response = requests.get('http://api.tildacdn.info/v1/getprojectslist/?publickey=%s&secretkey=%s' % (config.publickey,config.secretkey))
 answer = response.json()
 
 for project in answer['result']:
-    project_id = project['id'] + '-p'
-    #create Export folder and folders with project id
-    if os.path.exists(config.export_path):
-        pass
-    else:
-        os.mkdir(config.export_path)
-    if os.path.exists(config.export_path + '/' + project_id):
-        pass
-    else:
-        os.mkdir(config.export_path + '/' + project_id)
-    #get all pages id
-    responce = requests.get('http://api.tildacdn.info/v1/getpageslist/?publickey=%s&secretkey=%s&projectid=%s' % (config.publickey, config.secretkey, project_id))
+    project_id = project['id']
 
-    # print(responce.text)
-    answer = responce.json()
+
+
+    if os.path.exists(config_example.export_path):
+        pass
+    else:
+        os.mkdir(config_example.export_path)
+    if os.path.exists(config_example.export_path + '/' + project_id):
+        pass
+    else:
+        os.mkdir(config_example.export_path + '/' + project_id)
+    responce = requests.get('http://api.tildacdn.info/v1/getpageslist/?publickey=%s&secretkey=%s&projectid=%s' % (config.publickey,config.secretkey,project_id  ))
+
+    #print(responce.text)
+    answer= responce.json()
     pages_id = []
 
     for page in answer['result']:
         #print(page['id'])
-        pages_id.append(page['id'])
-        page_id = page['id']+'-s'
-        #create folders with page id and with js,css,html and images files
-        if os.path.exists(config.export_path + '/' + project_id + '/' + page_id):
-            pass
-        else:
-            os.mkdir(config.export_path + '/' + project_id + '/' + page_id)
+        #pages_id.append(page['id'])
 
-        if os.path.exists(config.export_path + '/' + project_id + '/' + page_id + '/css'):
-            pass
-        else:
-            os.mkdir(config.export_path + '/' + project_id + '/' + page_id + '/css')
+        css_path = config_example.export_path + '/' + project_id+'/'+ page['id']+ '/css'
+        js_path = config_example.export_path + '/' + project_id + '/' + page['id'] + '/js'
+        images_path = config_example.export_path + '/' + project_id + '/' + page['id'] + '/images'
 
-        if os.path.exists(config.export_path + '/' + project_id + '/' + page_id + '/html'):
-            pass
-        else:
-            os.mkdir(config.export_path + '/' + project_id + '/' + page_id + '/html')
+        if config_example.export == {}:
+            if os.path.exists(config_example.export_path + '/' + project_id + '/' + page['id']):
+                pass
+            else:
+                os.mkdir(config_example.export_path + '/' + project_id+'/'+ page['id'])
 
-        if os.path.exists(config.export_path + '/' + project_id + '/' + page_id + '/images'):
-            pass
-        else:
-            os.mkdir(config.export_path + '/' + project_id + '/' + page_id + '/images')
 
-        if os.path.exists(config.export_path + '/' + project_id + '/' + page_id + '/js'):
-            pass
+            if os.path.exists(css_path):
+                pass
+            else:
+                os.mkdir(css_path)
+
+
+            if os.path.exists(images_path):
+                pass
+            else:
+                os.mkdir(images_path)
+
+
+            if os.path.exists(js_path):
+                pass
+            else:
+                os.mkdir(js_path)
+
         else:
-            os.mkdir(config.export_path + '/' + project_id + '/' + page_id + '/js')
-        # get full export of page
-        responce = requests.get('http://api.tildacdn.info/v1/getpagefullexport/?publickey=%s&secretkey=%s&pageid=%s' % (config.publickey, config.secretkey, page['id']))
+            page_ID = config_example.page_ID
+            page_path = config_example.export_path + '/' + project_id + '/' + page['id']
+            if os.path.exists(page_path):
+                pass
+            else:
+                os.mkdir(page_path)
+            try:
+                images_path = config_example.export_path + '/' + project_id + '/' + page['id']+'/' + config_example.export[page_ID]['images_path']
+                #print(images_path)
+                if os.path.exists(images_path):
+                    pass
+                else:
+                    os.mkdir(images_path)
+            except FileNotFoundError:
+                images_path = config_example.export[page_ID]['images_path']
+                print(images_path)
+                if os.path.exists(images_path):
+                    pass
+                else:
+                    os.mkdir(images_path)
+
+            try:
+                css_path = config_example.export_path + '/' + project_id + '/' + page['id'] + '/' + config_example.export[page_ID]['css_path']
+
+                if os.path.exists(css_path):
+                    pass
+                else:
+                    os.mkdir(css_path)
+
+            except FileNotFoundError:
+                ccs_path = config_example.export[page_ID]['css_path']
+                if os.path.exists(css_path):
+                    pass
+                else:
+                    os.mkdir(css_path)
+
+            try:
+                js_path = config_example.export_path + '/' + project_id + '/' + page['id'] + '/' + config_example.export[page_ID]['js_path']
+
+                if os.path.exists(js_path):
+                    pass
+                else:
+                    os.mkdir(js_path)
+            except FileNotFoundError:
+                images_path = config_example.export[page_ID]['js_path']
+                if os.path.exists(js_path):
+                    pass
+                else:
+                    os.mkdir(js_path)
+
+
+        responce = requests.get('http://api.tildacdn.info/v1/getpagefullexport/?publickey=%s&secretkey=%s&pageid=%s' % (config.publickey,config.secretkey,page['id']))
         # pprint(responce.json()['result']['css'])
-
-        #all css files
         file_list_css = responce.json()['result']['css']
         for file in file_list_css:
-            urlretrieve(file['from'], (config.export_path + '/' + project_id + '/' + page_id + '/css/' + file['to']))
-        # all html files
+            urlretrieve(file['from'], (css_path + '/'+ file['to']))
+
         file_name = responce.json()['result']['filename']
-        file = open(config.export_path + '/' + project_id + '/' + page_id + '/html/' + file_name, 'w', encoding='utf-8')
+        file = open(config_example.export_path + '/' + project_id+'/'+ page['id'] + '/'+  file_name, 'w', encoding='utf-8')
         file_html = responce.json()['result']['html']
         if file_html != None:
             file.write(file_html)
         file.close()
-        # all images files
+
         file_list_images = responce.json()['result']['images']
         for file in file_list_images:
-            urlretrieve(file['from'], (config.export_path + '/' + project_id + '/' + page_id + '/' + 'images/' + file['to']))
-        # all js files
+            urlretrieve(file['from'], (images_path +'/'+ file['to']))
+
         file_list_js = responce.json()['result']['js']
         for file in file_list_js:
-            urlretrieve(file['from'], (config.export_path + '/' + project_id + '/' + page_id + '/' + 'js/' + file['to']))
+            urlretrieve(file['from'], (js_path +'/' + file['to']))
 
--- config_example.py
+++ config_example.py
@@ -2,3 +2,12 @@ publickey= '' #put your tilda public key
 secretkey=''  #put your tilda secret key
 
 export_path = 'Export'
+page_ID = 2832050
+
+export = {
+    page_ID:{
+        'css_path': 'css',
+        'js_path': '../../../js',
+        'images_path':'images'
+    }
+}
