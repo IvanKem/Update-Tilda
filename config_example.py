88db4b8a137001b1059d409ff30f85abb4fa0e3f
IvanKem <iv.kemaev@mail.ru>
2020-08-13 18:08:53 +0300

    Flask can get Get Request and write it in data.py(file)

--- config_example.py
+++ config_example.py
@@ -4,17 +4,4 @@ secretkey=''  #put your tilda secret key
 export_path = 'Export'
 
 
-export = {
-    2832050:{
-        'css_path': 'css',
-        'js_path': '../../../js',
-        'images_path':'images'
-    }
-}
-'''
-    2832051:{
-        'css_path': 'css',
-        'js_path': '../../../js',
-        'images_path':'images'
-    }'''
-
+export = {}
--- flask_server.py
+++ flask_server.py
@@ -1,24 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request,request, redirect
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    greeting = "Я вас не знаю :("
-    if "name" in request.args:
-        greeting = "Привет, {}".format(request.args['name'])
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'GET':
+        # You probably don't have args at this route with GET
+        # method, but if you do, you can access them like so:
+        pageid = request.args.get('pageid','')
+        file = open('data.py', 'w', encoding='utf-8')
+        if pageid != None:
+            file.write(pageid)
+        file.close()
+        return pageid
 
-    fruit = "Я не знаю, какой у вас любимый фрукт..."
-    if "fruit" in request.args:
-        fruit = "Ваш любимый фрукт - {}!".format(request.args['fruit'])
 
-    return """
-    <html>
-        <body>
-            <p>{}</p>
-            <p>{}</p>
-        </body>
-    </html>
-    """.format(greeting, fruit)
+
+    elif request.method == 'POST':
+        pageid = request.values.get('pageid')  # Your form's
+        return pageid
+
 
 app.run(debug=True, port=8080)
