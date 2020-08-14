23b8799edc11f2f5e997dfab29c4ee71462758e0
IvanKem <iv.kemaev@mail.ru>
2020-08-14 12:25:05 +0300

    Flask can get Get Request and write it in data.py(file) with 2 variables(page_id and project_id)

--- flask_server.py
+++ flask_server.py
@@ -5,14 +5,33 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
     if request.method == 'GET':
-        # You probably don't have args at this route with GET
-        # method, but if you do, you can access them like so:
-        pageid = request.args.get('pageid','')
+        page = "None"
+        if "pageid" in request.args:
+            page = request.args.get('pageid','')
+
+
+        project = 'None'
+        if "projectid" in request.args:
+            project = request.args.get('projectid','')
+
         file = open('data.py', 'w', encoding='utf-8')
-        if pageid != None:
-            file.write(pageid)
+        page_id = 'page_id =' + page
+        project_id = 'project_id='+project
+        if page != None:
+            file.write(page_id)
+            file.write('\n'+project_id)
         file.close()
-        return pageid
+
+
+
+        return """
+            <html>
+                <body>
+                    <p>{}</p>
+                    <p>{}</p>
+                </body>
+            </html>
+            """.format(page, project)
 
 
 
